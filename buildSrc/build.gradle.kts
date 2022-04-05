2a127f4a5462229ad70f1209f3b288d18b905871
flor-resoagli <florencia.resoagli@ing.austral.edu.ar>
2022-04-05 14:30:11 -0300

    added formatter, linter, coverage

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.github.eugenesy.scapegoat") version "0.2.0"
     id("org.scoverage") version "7.0.0"
     id("cz.alenkacz.gradle.scalafmt") version "1.16.2"
+    jacoco
 }
 
 repositories {
@@ -11,5 +12,20 @@ repositories {
 
 tasks {
     reportScoverage
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
 }
 
