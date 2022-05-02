404885b9144b8697eae3d74a9e88f03c07cb226d
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2022-05-02 10:35:27 -0300

    Added constructor parameter to RunningMode classes in tests

--- cli/src/test/scala/CliTesting.scala
+++ cli/src/test/scala/CliTesting.scala
@@ -8,24 +8,24 @@ class CliTesting {
   
   @Test
   def test01_executionModeShouldSucceed(): Unit = {
-     val runnedFile = new Cli().run(new FileReader("src/test/resources/test.txt"), "1.0", new ExecutionMode())
+     val runnedFile = new Cli().run(new FileReader("src/test/resources/test.txt"), "1.0", new ExecutionMode(Nil))
      assertTrue(true)
   }
   @Test
   def test02_executionModeShouldSucceed(): Unit = {
-    val runnedFile = new Cli().run(new FileReader("src/test/resources/test.txt"), "1.0", new ExecutionMode())
+    val runnedFile = new Cli().run(new FileReader("src/test/resources/test.txt"), "1.0", new ExecutionMode(Nil))
     assertTrue(true)
   }
 
 
   @Test
   def test_validationModeShouldSucceed(): Unit = {
-    val runnedFile = new Cli().run(new FileReader("src/test/resources/printString.txt"), "1.0", new ExecutionMode())
+    val runnedFile = new Cli().run(new FileReader("src/test/resources/printString.txt"), "1.0", new ExecutionMode(Nil))
     assertTrue(true)
   }
   @Test
   def test02_validationModeShouldSucceed(): Unit = {
-    val runnedFile = new Cli().run(new FileReader("src/test/resources/printString.txt"), "1.0", new ValidationMode())
+    val runnedFile = new Cli().run(new FileReader("src/test/resources/printString.txt"), "1.0", new ValidationMode(Nil))
     assertTrue(true)
   }
 
