cc6b971f78f70027b386ab33da1201bfbda97ba2
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2022-05-02 21:48:43 -0300

    Changed Observer List from scala.List to java.util.list

--- cli/src/main/scala/org/florresoagli/printscript/RunningMode.scala
+++ cli/src/main/scala/org/florresoagli/printscript/RunningMode.scala
@@ -1,31 +1,43 @@
 package org.florresoagli.printscript
 
 import org.florresoagli.printscript.VariableType
-
+import java.util
 import java.util.Observer
 import scala.collection.mutable.Map
+import scala.jdk.CollectionConverters._
+
+
 
 trait Observer {
-  def update(result: List[String]): Unit
-}
-case class PrintEmiterImpl() extends Observer {
 
-  def update(result: List[String]): Unit = {
-    result.foreach(println)
+  def update(result: java.util.List[java.lang.String]):Unit
+  def getList(): java.util.List[java.lang.String]
+
+}
+class PrintEmiterImpl extends Observer {
+
+ val  output: java.util.List[java.lang.String] = new java.util.ArrayList[java.lang.String]();
+
+  def update(result: java.util.List[java.lang.String]) = {
+    output.addAll(result)
+  }
+
+  def getList(): java.util.List[java.lang.String] =  {
+    return output
   }
 }
 
 
-case class ErrorEmitterImpl() extends Observer {
+class ErrorEmitterImpl extends Observer {
 
-  private var errors: List[String] = List()
+  val  errors: java.util.List[java.lang.String] = new java.util.ArrayList[java.lang.String]();
 
-  def update(result: List[String]): Unit = {
-    errors = errors ++ result
+  def update(result: java.util.List[java.lang.String]) = {
+    errors.addAll(result);
   }
 
-  def getErrors(): List[String] = {
-    errors
+  def getList(): java.util.List[java.lang.String] =  {
+    return errors
   }
 }
 
@@ -33,8 +45,8 @@ case class ErrorEmitterImpl() extends Observer {
 
 
 trait RunningMode{
-type InterpreterResult = (List[String], Map[String, (VariableType, Any)])
-type PrintResult = List[String]
+  type InterpreterResult = (List[String], Map[String, (VariableType, Any)])
+  type PrintResult = List[String]
 
   def run(input: InterpreterResult): List[String]
   def runError(message: List[String], stage: String): List[String]
@@ -45,13 +57,13 @@ class ExecutionMode(printObserver: Observer, errorObserver: Observer) extends Ru
 
   override def run(input: InterpreterResult): List[String] = {
     println(s"Finished process")
-    printObserver.update(input._1)
+    printObserver.update(input._1.asJava)
     val outputMessage = if(input._1.nonEmpty) "Finished process with output: " else "Finished process, no output"
     println(s"${outputMessage} \n${input._1.mkString("\n")}")
     List(outputMessage) ++ input._1
   }
   override def runError(message: List[String], stage: String): List[String] = {
-    errorObserver.update(message)
+    errorObserver.update(message.asJava)
     val errorMessage = "Error in stage: " + stage + "\n" + message.mkString("\n")
     println(errorMessage)
     message
@@ -63,14 +75,14 @@ class ValidationMode(printObserver: Observer, errorObserver: Observer) extends R
 
 
   override def run(input: InterpreterResult): List[String] = {
-    printObserver.update(input._1)
+    printObserver.update(input._1.asJava)
     val outputMessage = s"Validation succeeded, no syntax nor semantic errors found"
     println(outputMessage)
     List(outputMessage)
 
   }
   override def runError(message: List[String], stage:String): List[String] = {
-    errorObserver.update(message)
+    errorObserver.update(message.asJava)
     val errorMessage = s"Validation failed, errors found in $stage, error messages: "
     println(s"${errorMessage} ${message}" )
     List(errorMessage) ++ message
