b240094dd19151f61cd57392fc2dab71bbedf340
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-07-16 20:27:39 -0300

    Added gradle.yml

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     `kotlin-dsl`
-
     id("com.github.eugenesy.scapegoat") version "0.2.0"
     id("org.scoverage") version "7.0.0"
     id("cz.alenkacz.gradle.scalafmt") version "1.16.2"
@@ -40,7 +39,7 @@ tasks.jacocoTestReport {
 application {
     mainClass.set("cli.main.scala.Main")
 }
-repositories{
-    mavenCentral()
-    jcenter()
-}
+//repositories{
+//    mavenCentral()
+//    jcenter()
+//}
