5d585a36cf6e23c8a5e2ac97eda385ebc47fdf7e
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-07-19 12:38:34 -0300

    Change 1

--- buildSrc/src/main/kotlin/printscript.library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/printscript.library-conventions.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     `java-library`
     id("printscript.java-conventions")
-    `scala`
-    application
+    scala
+//    application
 }
 
 // Projects have the 'com.example' group by convention
 //group = "org.florresoagli"
 //version = "0.1.7"
+// change 1
+
+
+
